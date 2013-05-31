5179d80a75c2764e81ecebd6ff8b0ad26a047e4a
Reilly Watson <rwatson@kobo.com>
2013-05-31 19:12:41 -0400

    Add readability.  Doesn't work yet.

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 var http = require('http');
+var fs = require('fs');
+var Spooky = require('spooky');
+var readability = require('./readability.js');
 
 var app = http.createServer(handler);
+
+var readability = fs.readFileSync(__dirname + '/readability.js');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
 
+var get_clean_article = function(url, res) {
+	var spooky = new Spooky({
+	        casper: {
+	            logLevel: 'debug',
+	            verbose: true
+	        }
+	    }, function (err) {
+	        if (err) {
+	            e = new Error('Failed to initialize SpookyJS');
+	            e.details = err;
+	            throw e;
+	        }
+
+	        spooky.on('error', function (e) {
+	            console.error(e);
+	        });
+
+	        
+	        // Uncomment this block to see all of the things Casper has to say.
+	        // There are a lot.
+	        // He has opinions.
+	        spooky.on('console', function (line) {
+	            console.log(line);
+	        });
+	        
+
+	        spooky.on('log', function (log) {
+	            if (log.space === 'remote') {
+	                console.log(log.message.replace(/ \- .*/, ''));
+	            }
+	        });
+		spooky.start(url);
+//		spooky.thenEvaluate(function() {
+//			readability.init();
+//		});
+		spooky.thenEvaluate(function () {
+			res.end(this.getHTML());
+		});
+		spooky.run();
+	});
+};
+
 function handler (req, res) {
-	http.get("http://www.google.com/index.html", function(res) {
-		console.log("Got response: " + res.statusCode);
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
-	});
-	function (err, data) {
-		res.writeHead(200);
-		res.end(data);
-	});
+	get_clean_article('http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost', res);
 }
 
