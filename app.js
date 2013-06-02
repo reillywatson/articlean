71673a94da64d373dad9f30afec0c4d2342dabdc
Reilly Watson <rwatson@kobo.com>
2013-06-02 15:57:24 -0400

    Support gzip and deflate compression

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var phantom = require('phantom');
 var url = require('url');
+var zlib = require('zlib');
 
 var app = http.createServer(handler);
 
@@ -10,31 +11,47 @@ app.listen(port, function() {
 	console.log("Listening on " + port);
 });
 
-var get_clean_article = function(url, res, inlineImages) {
+var cleanup_html = function(inlineImages) {
+	readability.init();
+	if (inlineImages) {
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
+		var images = document.documentElement.getElementsByTagName('img');
+		for (var i = 0; i < images.length; i++) {
+			canvas.width = images[i].width;
+			canvas.height = images[i].height;
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			ctx.drawImage(images[i], 0, 0);
+			var url = images[i].src;
+			var dataURL=canvas.toDataURL('image/png');
+			images[i].src = dataURL;
+		}
+	}
+	return document.documentElement.innerHTML;
+}
+
+var compress = function(data, res, acceptEncoding, callback) {
+	var writeData = function(err, buffer) { res.end(buffer); }
+	if (acceptEncoding.match(/\bdeflate\b/)) {
+		res.writeHead(200, { 'content-encoding': 'deflate' });
+		zlib.deflateRaw(data, writeData);
+	} else if (acceptEncoding.match(/\bgzip\b/)) {
+		res.writeHead(200, { 'content-encoding': 'gzip' });
+		zlib.gzip(data, writeData);
+	} else {
+		res.writeHead(200, {});
+		writeData(null, data);
+	}
+}
+
+var get_clean_article = function(url, res, inlineImages, acceptEncoding) {
 	phantom.create(function(ph) {
 		return ph.createPage(function(page) {
 			page.set('settings.webSecurityEnabled', false);
 			return page.open(url, function(status) {
 				return page.injectJs('./readability.js', function() {
-					return page.evaluate(function(inlineImages) {
-						readability.init();
-						if (inlineImages) {
-							var canvas = document.createElement('canvas');
-							var ctx = canvas.getContext('2d');
-							var images = document.documentElement.getElementsByTagName('img');
-							for (var i = 0; i < images.length; i++) {
-								canvas.width = images[i].width;
-								canvas.height = images[i].height;
-								ctx.clearRect(0, 0, canvas.width, canvas.height);
-								ctx.drawImage(images[i], 0, 0);
-								var url = images[i].src;
-								var dataURL=canvas.toDataURL('image/png');
-								images[i].src = dataURL;
-							}
-						}
-						return document.documentElement.innerHTML;
-					}, function(result) {
-						res.end(result);
+					return page.evaluate(cleanup_html, function(html) {
+						compress(html, res, acceptEncoding);
 						return ph.exit();
 					}, inlineImages);
 				});
@@ -48,6 +65,10 @@ function handler (req, res) {
 	var article_url = url_parts.query.url;
 	var inline_images = url_parts.query.inlineImages === 'true';
 	var api_key = url_parts.query.apiKey;
+	var accept_encoding = req.headers['accept-encoding'];
+	if (!accept_encoding) {
+		accept_encoding = '';
+	}
 	if (api_key !== '1e203ad5a027436e9f72e1341cb801d9') {
 		res.writeHead('403');
 		res.end('Invalid API key!');
@@ -59,5 +80,5 @@ function handler (req, res) {
 		res.end('');
 		return;
 	}
-	get_clean_article(article_url, res, inline_images);
+	get_clean_article(article_url, res, inline_images, accept_encoding);
 }
