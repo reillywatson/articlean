5a6ebe6a4aef67932fce20c5a38d401f34a5abfb
Reilly Watson <rwatson@kobo.com>
2013-06-17 17:46:59 -0400

    Make Pinterest unlikely.  Fix Rolling Stone articles

--- readability.js
+++ readability.js
@@ -44,8 +44,8 @@ var readability = {
      * Defined up here so we don't instantiate them repeatedly in loops.
      **/
     regexps: {
-        unlikelyCandidates:    /combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|tweet|twitter/i,
-        okMaybeItsACandidate:  /and|article|body|column|main|shadow/i,
+        unlikelyCandidates:    /combx|comment|community|disqus|extra|foot|header|menu|remark|rss|shoutbox|sidebar|sponsor|ad-break|agegate|pagination|pager|popup|tweet|twitter|pinterest/i,
+        okMaybeItsACandidate:  /and|article|body|column|main|shadow|content/i,
         positive:              /article|body|content|entry|hentry|main|page|pagination|post|text|blog|story/i,
         negative:              /combx|comment|com-|contact|foot|footer|footnote|masthead|media|meta|outbrain|promo|related|scroll|shoutbox|sidebar|sponsor|shopping|tags|tool|widget/i,
         extraneous:            /print|archive|comment|discuss|e[\-]?mail|share|reply|all|login|sign|single/i,
